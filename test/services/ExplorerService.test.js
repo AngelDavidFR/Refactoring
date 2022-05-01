0dab6c4890db8693aa48c7c99c8a4e823d1fe889
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-05-01 14:43:44 -0500

    Eslint instalación y ejecución

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,13 +1,13 @@
 const Reader = require("../../lib/utils/Reader");
-const ExplorerService=require("../../lib/services/ExplorerService")
+const ExplorerService=require("../../lib/services/ExplorerService");
 describe("Test for ExplorerService", () => {
     test("Test for method of the class ExplorerServices", () => {
         const explorers = Reader.readJsonFile("explorers.json");
-        expect(ExplorerService.filterByMission(explorers, "node").length).toBe(10)
-        expect(ExplorerService.getAmountOfExplorersByMission(explorers, "node")).toBe(10)
-        expect((ExplorerService.getExplorersUsernamesByMission(explorers, "node"))[0]).toBe("ajolonauta1")
-        expect(ExplorerService.getExplorersUsernamesByMission(explorers, "node")).toEqual(['ajolonauta1',
-        'ajolonauta2','ajolonauta3','ajolonauta4','ajolonauta5','ajolonauta11','ajolonauta12','ajolonauta13','ajolonauta14','ajolonauta15'])
+        expect(ExplorerService.filterByMission(explorers, "node").length).toBe(10);
+        expect(ExplorerService.getAmountOfExplorersByMission(explorers, "node")).toBe(10);
+        expect((ExplorerService.getExplorersUsernamesByMission(explorers, "node"))[0]).toBe("ajolonauta1");
+        expect(ExplorerService.getExplorersUsernamesByMission(explorers, "node")).toEqual(["ajolonauta1",
+            "ajolonauta2","ajolonauta3","ajolonauta4","ajolonauta5","ajolonauta11","ajolonauta12","ajolonauta13","ajolonauta14","ajolonauta15"]);
         
 
         
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,15 +1,15 @@
 
-const FizzbuzzService=require("../../lib/services/FizzbuzzService")
+const FizzbuzzService=require("../../lib/services/FizzbuzzService");
 describe("Test for FizzbuzzService", () => {
     test("Application of the ExplorerService to the list of explorers", () => {
-        const explorer1 = {name: "Explorer1", score: 1}
-        const explorer3 = {name: "Explorer3", score: 3}
-        const explorer5 = {name: "Explorer5", score: 5}
-        const explorer15 = {name: "Explorer15", score: 15}
-        expect(FizzbuzzService.applyValidationInExplorer(explorer1)).toEqual({name: "Explorer1", score: 1, trick: 1})
-        expect(FizzbuzzService.applyValidationInExplorer(explorer3)).toEqual({name: "Explorer3", score: 3, trick: "FIZZ"})
-        expect(FizzbuzzService.applyValidationInExplorer(explorer5)).toEqual({name: "Explorer5", score: 5, trick: "BUZZ"})
-        expect(FizzbuzzService.applyValidationInExplorer(explorer15)).toEqual({name: "Explorer15", score: 15, trick: "FIZZBUZZ"})
+        const explorer1 = {name: "Explorer1", score: 1};
+        const explorer3 = {name: "Explorer3", score: 3};
+        const explorer5 = {name: "Explorer5", score: 5};
+        const explorer15 = {name: "Explorer15", score: 15};
+        expect(FizzbuzzService.applyValidationInExplorer(explorer1)).toEqual({name: "Explorer1", score: 1, trick: 1});
+        expect(FizzbuzzService.applyValidationInExplorer(explorer3)).toEqual({name: "Explorer3", score: 3, trick: "FIZZ"});
+        expect(FizzbuzzService.applyValidationInExplorer(explorer5)).toEqual({name: "Explorer5", score: 5, trick: "BUZZ"});
+        expect(FizzbuzzService.applyValidationInExplorer(explorer15)).toEqual({name: "Explorer15", score: 15, trick: "FIZZBUZZ"});
 
     });
 });
