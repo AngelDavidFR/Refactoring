a78b1aa762e508b6d43bee83bcc06e8834350816
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-05-01 13:11:18 -0500

    ExplorerService test

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,16 @@
+const Reader = require("../../lib/utils/Reader");
+const ExplorerService=require("../../lib/services/ExplorerService")
+describe("Test for ExplorerService", () => {
+    test("Create a Reader object", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        expect(ExplorerService.filterByMission(explorers, "node").length).toBe(10)
+        expect(ExplorerService.getAmountOfExplorersByMission(explorers, "node")).toBe(10)
+        expect((ExplorerService.getExplorersUsernamesByMission(explorers, "node"))[0]).toBe("ajolonauta1")
+        expect(ExplorerService.getExplorersUsernamesByMission(explorers, "node")).toEqual(['ajolonauta1',
+        'ajolonauta2','ajolonauta3','ajolonauta4','ajolonauta5','ajolonauta11','ajolonauta12','ajolonauta13','ajolonauta14','ajolonauta15'])
+        
+
+        
+
+    });
+});
