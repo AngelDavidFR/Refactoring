1ae615af326ec2d1208b966b50060c33e04d967c
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-05-01 10:49:46 -0500

    Crear metodos

--- app.js
+++ app.js
@@ -74,3 +74,16 @@ const ExplorerService=require("./lib/services/ExplorerService")
 console.log(ExplorerService.filterByMission(explorers3, "node"))
 console.log(ExplorerService.getAmountOfExplorersByMission(explorers3, "node"))
 console.log(ExplorerService.getExplorersUsernamesByMission(explorers3, "node"))
+
+const FizzbuzzService=require("./lib/services/FizzbuzzService")
+const explorer1 = {name: "Explorer1", score: 1}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1)) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3)) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer5)) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15)) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
