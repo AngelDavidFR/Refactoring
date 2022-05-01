705988b62f12ba020bc21eb805374ad665346fe8
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-05-01 13:48:13 -0500

    action prueba

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -9,7 +9,7 @@ describe("Test for FizzbuzzService", () => {
         expect(FizzbuzzService.applyValidationInExplorer(explorer1)).toEqual({name: "Explorer1", score: 1, trick: 1})
         expect(FizzbuzzService.applyValidationInExplorer(explorer3)).toEqual({name: "Explorer3", score: 3, trick: "FIZZ"})
         expect(FizzbuzzService.applyValidationInExplorer(explorer5)).toEqual({name: "Explorer5", score: 5, trick: "BUZZ"})
-        expect(FizzbuzzService.applyValidationInExplorer(explorer15)).toEqual({name: "Explorer15", score: 15, trick: "FIZZBUZ"})
+        expect(FizzbuzzService.applyValidationInExplorer(explorer15)).toEqual({name: "Explorer15", score: 15, trick: "FIZBUZ"})
 
     });
 });
